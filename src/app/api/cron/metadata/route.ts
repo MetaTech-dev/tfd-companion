94decfc6c0a26188b9b2561efa4d475aa0cd1cf2
Matt Whitney <matt@metatech.dev>
2024-08-05 03:41:21 -0700

    feat(api): Add error handling and metadata upserts
    Added error handling to catch errors during metadata upsert operations. Updated the function to return JSON responses based on success or failure.

--- src/app/api/cron/metadata/route.ts
+++ src/app/api/cron/metadata/route.ts
@@ -1,36 +1,46 @@
 import { NextResponse } from "next/server";
 import * as metadataRequests from "@/data-sources/nexon/metadata";
 import * as queries from "@/db/queries";
-export async function GET() {
-  await queries.clearDb();
 
-  const metadataDescendants = await metadataRequests.getMetadataDescendant();
-  await queries.upsertManyMetadataDescendant(metadataDescendants);
-  console.log("Descendants upserted");
-  const metadataWeapons = await metadataRequests.getMetadataWeapon();
-  await queries.upsertManyMetadataWeapon(metadataWeapons);
-  console.log("Weapons upserted");
-  const metadataModules = await metadataRequests.getMetadataModule();
-  await queries.upsertManyMetadataModule(metadataModules);
-  console.log("Modules upserted");
-  const metadataReactors = await metadataRequests.getMetadataReactor();
-  await queries.upsertManyMetadataReactor(metadataReactors);
-  console.log("Reactors upserted");
-  const metadataExternalComponents =
-    await metadataRequests.getMetadataExternalComponent();
-  await queries.upsertManyMetadataExternalComponent(metadataExternalComponents);
-  console.log("External Components upserted");
-  const metadataRewards = await metadataRequests.getMetadataReward();
-  await queries.upsertManyMetadataReward(metadataRewards);
-  console.log("Rewards upserted");
-  const metadataStats = await metadataRequests.getMetadataStat();
-  await queries.upsertManyMetadataStat(metadataStats);
-  console.log("Stats upserted");
-  const metadataVoidBattles = await metadataRequests.getMetadataVoidBattle();
-  await queries.upsertManyMetadataVoidBattle(metadataVoidBattles);
-  console.log("Void Battles upserted");
-  const metadataTitles = await metadataRequests.getMetadataTitle();
-  await queries.upsertManyMetadataTitle(metadataTitles);
-  console.log("Titles upserted");
-  return NextResponse.json({ ok: true });
+export async function GET() {
+  try {
+    await queries.clearDb();
+
+    const metadataDescendants = await metadataRequests.getMetadataDescendant();
+    await queries.upsertManyMetadataDescendant(metadataDescendants);
+    console.log("Descendants upserted");
+    const metadataWeapons = await metadataRequests.getMetadataWeapon();
+    await queries.upsertManyMetadataWeapon(metadataWeapons);
+    console.log("Weapons upserted");
+    const metadataModules = await metadataRequests.getMetadataModule();
+    await queries.upsertManyMetadataModule(metadataModules);
+    console.log("Modules upserted");
+    const metadataReactors = await metadataRequests.getMetadataReactor();
+    await queries.upsertManyMetadataReactor(metadataReactors);
+    console.log("Reactors upserted");
+    const metadataExternalComponents =
+      await metadataRequests.getMetadataExternalComponent();
+    await queries.upsertManyMetadataExternalComponent(
+      metadataExternalComponents
+    );
+    console.log("External Components upserted");
+    const metadataRewards = await metadataRequests.getMetadataReward();
+    await queries.upsertManyMetadataReward(metadataRewards);
+    console.log("Rewards upserted");
+    const metadataStats = await metadataRequests.getMetadataStat();
+    await queries.upsertManyMetadataStat(metadataStats);
+    console.log("Stats upserted");
+    const metadataVoidBattles = await metadataRequests.getMetadataVoidBattle();
+    await queries.upsertManyMetadataVoidBattle(metadataVoidBattles);
+    console.log("Void Battles upserted");
+    const metadataTitles = await metadataRequests.getMetadataTitle();
+    await queries.upsertManyMetadataTitle(metadataTitles);
+    console.log("Titles upserted");
+
+    return NextResponse.json({ ok: true });
+  } catch (e) {
+    const error = e as Error;
+    console.error(error);
+    return NextResponse.json({ ok: false, error: error.message });
+  }
 }
